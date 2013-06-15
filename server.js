54d4e7a38e87ce0947803070099d74af799eabc3
Sachin Panvalkar <sachin.panvalkar@live.com>
2013-06-15 06:18:22 -0700

    Changed response of web server

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World from Sachin\n');
+    res.end('Hello World from Azure and Sachin, this is cool stuff \n');
 }).listen(port);
